7513530a3dcf2a21cc58f74b42a34709e5354357
An39nA0219 <an39na10282004@yahoo.co.jp>
2021-09-28 12:30:14 +0900

    add encrypt/decrypt function

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+  def decrypt(data)
+    key = "\x9B)\xF7\x857U\xDE\xDD\xF87\xBA\xEF\xDD\xBF\xD1G?\x01\xE9\x86\\\xC4KC\x1E\xDF\f\x82 \x11\t\xE2"
+    crypt = ActiveSupport::MessageEncryptor.new(key)
+    crypt.decrypt_and_verify(data)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  before_save :encrypt_email
+
+  def encrypt_email
+    key = "\x9B)\xF7\x857U\xDE\xDD\xF87\xBA\xEF\xDD\xBF\xD1G?\x01\xE9\x86\\\xC4KC\x1E\xDF\f\x82 \x11\t\xE2"
+    crypt = ActiveSupport::MessageEncryptor.new(key)
+    encrypted_data = crypt.encrypt_and_sign(email)
+    self.email = encrypted_data
+  end
 end
